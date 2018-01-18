628a6aa8ce9938b396069e0f64e3fe121fa58579
Shravan Kumar <32629846+shravanbaloji@users.noreply.github.com>
2018-01-18 12:27:52 +0530

    Add files via upload

---
+++ CodilityProb9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProb8
+{
+    class Program
+    {
+        public int Solution(int[] A) {
+            int orginal = 0, sum = 0, min = 0;
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                orginal = orginal + A[i];
+                sum = sum + Math.Abs(orginal);
+                Console.WriteLine(orginal+" "+sum);
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] A = { };
+        }
+    }
+}
