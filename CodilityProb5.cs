581c1f5ca7a6fc2e4671b1efcb0a630e534d4463
Shravan Kumar <32629846+shravanbaloji@users.noreply.github.com>
2018-01-18 12:29:48 +0530

    Add files via upload

---
+++ CodilityProb5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProb5
+{
+    class Program
+    {
+        public int Solution(int[] A)
+        {
+
+            Dictionary<int, int> B = new Dictionary<int, int>();
+            foreach (int item in A)
+            {
+                if (!B.ContainsKey(item))
+                    B.Add(item, 1);
+                else
+                {
+                    int count = B[item];
+                    B.Remove(item);
+                    B.Add(item, ++count);
+                    if (count >= (Math.Ceiling(Convert.ToDecimal(A.Length) / 2)))
+                    {
+                        Console.WriteLine("Leader = "+item);
+                        return 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] A = { 2, 5, 2, 1, 3, 5, 2, 2, 5, 5, 5, 5 };
+            Console.WriteLine(p.Solution(A));
+        }
+    }
+}
