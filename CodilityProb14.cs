975dbdf0b11da7f84c117c3f6e741bdbf3ae1a88
Shravan Kumar <32629846+shravanbaloji@users.noreply.github.com>
2018-01-19 10:49:41 +0530

    Add files via upload

---
+++ CodilityProb14.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProb14
+{
+    class Program
+    {
+        public int Solution(int A, int B)
+        {
+            int Rem = A * B, count = 0;
+            while (Rem != 0)
+            {
+                if (Rem % 2 != 0)
+                {
+                    count++;
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+                Rem = Rem / 2;
+                
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int A, B;
+            Console.WriteLine("Enter Any Two Integers");
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nNumber of 1's  =  " + p.Solution(A, B));
+        }
+    }
+}
